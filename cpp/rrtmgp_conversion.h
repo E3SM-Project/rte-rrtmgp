7bfa10882f2904df1ce4b84949981b99240585b3
akashdhruv <akashdhruv@gwmail.gwu.edu>
2026-02-14 23:17:32 -0800

    address copilot concerns

--- cpp/rrtmgp_conversion.h
+++ cpp/rrtmgp_conversion.h
@@ -507,8 +507,10 @@ void unflatten_idx(const int idx, const Kokkos::Array<int, 4>& dims, int& i, int
   {                                                     \
     const int n1_int = static_cast<int>(n1), n2_int = static_cast<int>(n2);  \
     Kokkos::Array<int, 2> dims_fmk_internal = {n1_int, n2_int};         \
-    const int dims_fmk_internal_tot = (n1_int)*(n2_int);                        \
-    Kokkos::parallel_for(dims_fmk_internal_tot, KOKKOS_LAMBDA (int idx_fmk_internal) { \
+    size_t dims_fmk_internal_tot = static_cast<size_t>(n1_int) * static_cast<size_t>(n2_int); \
+    if(dims_fmk_internal_tot > INT_MAX) Kokkos::abort("FLATTEN_MD_KERNEL2: too big"); \
+    Kokkos::parallel_for(static_cast<int>(dims_fmk_internal_tot), \
+      KOKKOS_LAMBDA (int idx_fmk_internal) { \
       int i1, i2;                                                     \
       conv::unflatten_idx<LayoutT>(idx_fmk_internal, dims_fmk_internal, i1, i2); \
       kernel;                                                           \
@@ -519,9 +521,12 @@ void unflatten_idx(const int idx, const Kokkos::Array<int, 4>& dims, int& i, int
   {                                                              \
     const int n1_int = static_cast<int>(n1), n2_int = static_cast<int>(n2);   \
     const int n3_int = static_cast<int>(n3);          \
-    Kokkos::Array<int, 3> dims_fmk_internal = {n1_int, n2_int, n3_int};      \
-    const int dims_fmk_internal_tot = (n1_int)*(n2_int)*(n3_int);                   \
-    Kokkos::parallel_for(dims_fmk_internal_tot, KOKKOS_LAMBDA (int idx_fmk_internal) { \
+    Kokkos::Array<int, 3> dims_fmk_internal = {n1_int, n2_int, n3_int}; \
+    size_t dims_fmk_internal_tot = static_cast<size_t>(n1_int) * static_cast<size_t>(n2_int) * \
+                                   static_cast<size_t>(n3_int); \
+    if(dims_fmk_internal_tot > INT_MAX) Kokkos::abort("FLATTEN_MD_KERNEL3: too big"); \
+    Kokkos::parallel_for(static_cast<int>(dims_fmk_internal_tot), \
+      KOKKOS_LAMBDA (int idx_fmk_internal) { \
       int i1, i2, i3;                                                 \
       conv::unflatten_idx<LayoutT>(idx_fmk_internal, dims_fmk_internal, i1, i2, i3); \
       kernel;                                                           \
@@ -533,8 +538,11 @@ void unflatten_idx(const int idx, const Kokkos::Array<int, 4>& dims, int& i, int
     const int n1_int = static_cast<int>(n1), n2_int = static_cast<int>(n2);   \
     const int n3_int = static_cast<int>(n3), n4_int = static_cast<int>(n4); \
     Kokkos::Array<int, 4> dims_fmk_internal = {n1_int, n2_int, n3_int, n4_int};         \
-    const int dims_fmk_internal_tot = (n1_int)*(n2_int)*(n3_int)*(n4_int);              \
-    Kokkos::parallel_for(dims_fmk_internal_tot, KOKKOS_LAMBDA (int idx_fmk_internal) { \
+    size_t dims_fmk_internal_tot = static_cast<size_t>(n1_int) * static_cast<size_t>(n2_int) * \
+                                   static_cast<size_t>(n3_int) * static_cast<size_t>(n4_int); \
+    if(dims_fmk_internal_tot > INT_MAX) Kokkos::abort("FLATTEN_MD_KERNEL4: too big"); \
+    Kokkos::parallel_for(static_cast<int>(dims_fmk_internal_tot), \
+      KOKKOS_LAMBDA (int idx_fmk_internal) {                      \
       int i1, i2, i3, i4;                                             \
       conv::unflatten_idx<LayoutT>(idx_fmk_internal, dims_fmk_internal, i1, i2, i3, i4); \
       kernel;                                                           \
